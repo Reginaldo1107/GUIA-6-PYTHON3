51f8340055b6030a87ae380e51b1428e2dbf884b
Reginaldo1107 <reginaldohinojosabaldera@gmail.com>
2023-10-02 11:32:32 -0300

    EJE 1 ejercicio2.py

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,1 +1,10 @@
+#1. problema_imprimir_saludo (in nombre: String) {
+#requiere: { True }
+#asegura: {imprime “Hola < nombre >”por pantalla}
+#}
 
+def imprimir_saludo (nombre : str) ->None:
+    print("HOLA " + nombre)
+
+
+imprimir_saludo("NALDO")
